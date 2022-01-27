6624bef18a7d9d4f18712915ee83647cf907e2aa
elegs99 <71243839+elegs99@users.noreply.github.com>
2022-01-27 17:05:43 -0500

    Working V1
    Primes are found and counted/summed by 8 threads. Tracking 10 max primes not yet implmented.

--- ParallelPrimeCounter.java
+++ ParallelPrimeCounter.java
@@ -1,35 +1,66 @@
 import java.io.*;
 import java.util.*;
+import java.util.concurrent.atomic.AtomicInteger;
 
-public class ParrallelPrimeCounter {
+public class ParallelPrimeCounter {
+
+  static class Counter {
+      private AtomicInteger index = new AtomicInteger(1);
+
+      public int increment() {
+         return index.getAndAdd(2);
+      }
+
+      public int value() {
+         return index.get();
+      }
+   }
+
 	public static void main(String[] args) {
-    FileWriter f = new FileWriter("primes.txt");
+    long target = (long)Math.pow(10, 8);
     long start = System.currentTimeMillis();
-
-    long end = System.currentTimeMillis();
-    f.write(end - start);
-    f.close();
-  }
-}
-public class IsPrime extends Thread {
-  public void isPrime() {
-
-
-  }
-}
-public class AtomicCounter {
-  private final AtomicInteger count = new AtomicInteger(0);
-
-  public int getValue() {
-    return count.get();
-  }
-  public void increment() {
-    while(1==1) {
-      int preVal = getValue();
-      int newVal = preVal + 1;
-      if(count.compareAndSet(preVal, newVal)) {
-        return;
+    int primeCount = 1; //acounting for the 2 we are skipping
+    long primeSum = 2;
+    Counter index = new Counter();
+    IsPrime[] threads = new IsPrime[8];
+    for (int i = 0; i < 8; i++) {
+      threads[i] = new IsPrime();
+      threads[i].start();
+    }
+    while (index.value() <= target) {
+      for (int i = 0; i < 8; i++) {
+        if (index.value() <= target) {
+          if (threads[i].IsPrime(index.increment())) {
+            primeSum += index.value()-2;
+            primeCount++;
+            // capture all primes add and save last 10 max. System.out.println(index.value()-1);
+          }
+        }
       }
     }
+
+    long end = System.currentTimeMillis();
+    try {
+      FileWriter f = new FileWriter("primes.txt");
+      f.write(String.valueOf((int)(end - start)) + " ms\n");
+      f.write(String.valueOf(primeCount) + " total count of prime numbers\n");
+      f.write(String.valueOf(primeSum) + " total sum of prime numbers\n");
+      f.close();
+    } catch (IOException e) {
+      System.out.println("error with file IO");
+      e.printStackTrace();
+    }
+  }
+}
+
+class IsPrime extends Thread {
+  public boolean IsPrime(int currVal) {
+    if (currVal < 2) return false;
+    if (currVal == 2 || currVal == 3) return true;
+    long primeCeiling = (long)Math.sqrt(currVal)+1;
+    for (int checkVal = 2; checkVal <= primeCeiling; checkVal++) {
+      if (currVal % checkVal == 0) return false;
+    }
+    return true;
   }
 }
