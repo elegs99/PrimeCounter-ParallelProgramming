1293f46f7fea2f22f39738ba998535015ffa0840
elegs99 <71243839+elegs99@users.noreply.github.com>
2022-01-28 16:50:41 -0500

    small updates to prime capture

--- ParallelPrimeCounter.java
+++ ParallelPrimeCounter.java
@@ -18,21 +18,23 @@ public class ParallelPrimeCounter {
 
 	public static void main(String[] args) {
     long target = (long)Math.pow(10, 8);
-    long start = System.currentTimeMillis();
-    int primeCount = 1; //acounting for the 2 we are skipping
-    long primeSum = 2;
+    int primeCount = 1; // initial value accounts for the 2 we are skipping
+    long primeSum = 2; // initial value accounts for the 2 we are skipping
     Queue<Integer> maxPrimesList = new PriorityQueue<>(10);
 
     Counter index = new Counter();
-    IsPrime[] threads = new IsPrime[8];
+    long start = System.currentTimeMillis();
+    IsPrime[] threads = new IsPrime[8]; // spawning 8 threads
     for (int i = 0; i < 8; i++) {
       threads[i] = new IsPrime();
       threads[i].start();
     }
+    // Assign each thread every odd number from 1 to 10^8 using atomic counter to keep track of concurrency
     while (index.value() <= target) {
       for (int i = 0; i < 8; i++) {
         if (index.value() <= target) {
           if (threads[i].IsPrime(index.increment())) {
+            // if thread found prime value add that value to sum and increment prime count by 1
             int primeVal = index.value()-2;
             primeSum += primeVal;
             primeCount++;
@@ -54,6 +56,7 @@ public class ParallelPrimeCounter {
     }
 
     long end = System.currentTimeMillis();
+    // printing values to text file
     try {
       FileWriter f = new FileWriter("primes.txt");
       f.write(String.valueOf((int)(end - start)/1000) + " Seconds");
@@ -71,7 +74,7 @@ public class ParallelPrimeCounter {
     }
   }
 }
-
+// thread class that returns t/f depending if given value is prime or not
 class IsPrime extends Thread {
   public boolean IsPrime(int currVal) {
     if (currVal < 2) return false;
